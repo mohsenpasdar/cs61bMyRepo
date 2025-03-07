111547faa228f8d968a92628cb337e0f628df604
mohsen <mohsenput2@gmail.com>
2025-03-06 20:03:33 -0500

    HW0B - MapExercises and Dessert

--- hw0b/src/MapExercises.java
+++ hw0b/src/MapExercises.java
@@ -1,26 +1,40 @@
+import edu.princeton.cs.algs4.In;
+import org.apache.commons.collections.map.HashedMap;
+
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class MapExercises {
     /** Returns a map from every lower case letter to the number corresponding to that letter, where 'a' is
      * 1, 'b' is 2, 'c' is 3, ..., 'z' is 26.
      */
     public static Map<Character, Integer> letterToNum() {
-        // TODO: Fill in this function.
-        return null;
+        Map<Character, Integer> result = new HashMap<>();
+        for (int i = 0; i <= 25; i++) {
+            result.put((char) ('a' + i), i + 1);
+        }
+        return result;
     }
 
     /** Returns a map from the integers in the list to their squares. For example, if the input list
      *  is [1, 3, 6, 7], the returned map goes from 1 to 1, 3 to 9, 6 to 36, and 7 to 49.
      */
     public static Map<Integer, Integer> squares(List<Integer> nums) {
-        // TODO: Fill in this function.
-        return null;
+        Map<Integer, Integer> result = new HashMap<>();
+        for (int num: nums) {
+            result.put(num, num * num);
+        }
+        return result;
     }
 
     /** Returns a map of the counts of all words that appear in a list of words. */
     public static Map<String, Integer> countWords(List<String> words) {
-        // TODO: Fill in this function.
-        return null;
+        Map<String, Integer> result = new TreeMap<>();
+        for (String word: words) {
+            result.put(word, result.getOrDefault(word, 0) + 1);
+        }
+        return result;
     }
 }
